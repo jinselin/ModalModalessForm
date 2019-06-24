c7a1ba0c21405f1482ed24a653b646dd8c85f9af
selin516 <selin516@naver.com>
2019-06-24 13:51:34 +0900

    #1 Modal & Modaless 창 띄우기 by 3119 진세린

--- ModalModalessForm/FormModaless.cs
+++ ModalModalessForm/FormModaless.cs
@@ -9,16 +9,11 @@ using System.Windows.Forms;
 
 namespace ModalModalessForm
 {
-    public partial class FormMain : Form
+    public partial class FormModaless : Form
     {
-        public FormMain()
+        public FormModaless()
         {
             InitializeComponent();
-        }
-
-        private void 이프로그램은ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
